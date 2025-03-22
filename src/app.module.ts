665de84311d1453aa05762540bf89fc447afb177
rezarayan <rezarayan78@gmail.com>
2025-03-22 13:05:00 +0330

    Add: initial of student actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,27 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from "@nestjs/graphql";
+import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type:"mongodb",
-      appname:"e-learn",
-      synchronize:true,
-      entities:[
-        Lesson
-      ]
+      type: 'mongodb',
+      url: 'mongodb://localhost/e-learn',
+      synchronize: true,
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      autoSchemaFile:true,
+      autoSchemaFile: true,
       driver: ApolloDriver,
       playground: true,
     }),
-    LessonModule
+    LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
